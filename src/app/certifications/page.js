f0f41336276be040742c5883332ab253d9fba48d
Kunal Walavalkar <kunalw2002@gmail.com>
2024-01-02 17:52:45 +0530

    Update page.js

--- src/app/certifications/page.js
+++ src/app/certifications/page.js
@@ -56,7 +56,7 @@ const Certifications = () => {
 							<Button
 								button=""
 								buttonClassName="google clickable pages"
-								buttonHref="public/assets/certs/Google_Cybersecurity_Professional_Certificate.pdf"
+								buttonHref="/assets/certs/Google_Cybersecurity_Professional_Certificate.pdf"
 								target="_blank"
 								ariaLabel="Check out my Google Cybersecurity certificate"
 							/>
