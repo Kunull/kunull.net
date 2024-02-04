bc69d21de1d6c955b9e6d2f199156a68f012b8c5
Kunal Walavalkar <kunalw2002@gmail.com>
2024-02-04 17:15:18 +0530

    Update page.js

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,9 +3,9 @@ import Footer from '../../components/footer/footer';
 import Button from '../../components/button/button';
 
 export const metadata = {
-  title: 'Kunal Walavalkar | Projects',
+  title: 'Projects | Kunal Walavalkar',
   openGraph: {
-    title: 'Kunal Walavalkar | Projects',
+    title: 'Projects | Kunal Walavalkar',
   },
 }
 
