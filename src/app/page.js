5abef8188ba8b632f6153a11f15eb6efaafc67c5
Kunal Walavalkar <kunalw2002@gmail.com>
2024-03-18 23:21:10 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ import Footer from '../components/footer/footer';
 // import profilepic2 from '../assets/images/image(1).png';
 
 export const metadata = {
-  title: 'Kunull | Kunal Walavalkar',
+  title: 'KuNull | Kunal Walavalkar',
   openGraph: {
-    title: 'Kunull | Kunal Walavalkar',
+    title: 'KuNull | Kunal Walavalkar',
   },
 }
 
