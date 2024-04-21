4db2336bc5bda2d63d5766e8d2120a9a96d4d1f3
Kunal W <kunalw2002@gmail.com>
2024-04-21 09:59:53 +0530

    Update page.js

--- src/app/certifications/page.js
+++ src/app/certifications/page.js
@@ -103,7 +103,7 @@ const Certifications = () => {
 						<div className="capital">
 							<Button
 								button=""
-								buttonClassName="nse clickable pages"
+								buttonClassName="nse1 clickable pages"
 								buttonHref="/assets/certifications/NSE-Certification.pdf"
 								target="_blank"
 								ariaLabel="Check out my NSE certificate"
