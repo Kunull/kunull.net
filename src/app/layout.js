0a28796123b3bb7e68ee4cabe8a798f2550d480d
Kunal Walavalkar <kunalw2002@gmail.com>
2024-05-14 17:42:20 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,9 +34,11 @@ export default function RootLayout({ children }) {
         <div className='content-wrapper-front'>
           {children}
         </div>
-        // <div className='content-wrapper-back'>
-        //   {children}
-        // </div>
+  {/*   
+        <div className='content-wrapper-back'>
+          {children}
+        </div>
+  */}
 
       </body>
     </html>
