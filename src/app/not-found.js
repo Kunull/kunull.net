00c9894d3f72469530e1be901669c2ce9016c356
Kunal Walavalkar <kunalw2002@gmail.com>
2024-05-26 17:42:24 +0530

    Create not-found.js

---
+++ src/app/not-found.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Button from '../components/button/button';
+
+export default function NotFound() {
+  return (
+    <main className="capital">
+      <p className='four-o-four'>
+        <span className="">4</span>
+        <Button 
+              button="" 
+              buttonClassName="zero clickable pages" 
+              buttonHref="/" 
+              ariaLabel="Go to my Projects page"
+        />
+        <span className="">4</span>
+      </p>
+    </main>
+  );
+}
