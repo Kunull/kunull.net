c600161a1db2b6abba95216c161ba8f3a495c57f
Kunal Walavalkar <kunalw2002@gmail.com>
2024-05-30 22:51:23 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -52,7 +52,7 @@ const Home = () => {
 					
      					</span>
 */}	
-					<span className="personal">, A COMPUTER ENGINEERING AND CYBER SECURITY STUDENT.</span>
+					<span className="personal">, A CYBER SECURITY RESEARCHER.</span>
 {/*					
 					<span className="personal">, A PENETRATION TESTER BY PROFESSION AND CVE RESEARCHER BY AVOCATION.</span>
 */}
