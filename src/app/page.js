d1dc70c14a778280bdde3bb4bc868f2ce47f41ee
Kunal Walavalkar <kunalw2002@gmail.com>
2024-07-06 18:36:31 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ const Home = () => {
 				</p>								
 								
 				<p className='spacing'>
-					<span className="personal">IN MY FREE TIME I SOLVE AND CREATE </span>
+					<span className="personal">IN MY FREE TIME I CREATE </span>
 					<Button 
 						button="" 
 						buttonClassName="write-ups clickable pages" 
@@ -94,7 +94,7 @@ const Home = () => {
 						target="_blank"
 						ariaLabel="Go to my write-ups"
 					/>
-					<span className="personal"> FOR CTFS AND BOOT2ROOT MACHINES. I ALSO SHARE MY LEARNINGS AND EXPERIENCES ON MY </span>
+					<span className="personal"> FOR CTF CHALLENGES AND BOOT2ROOT MACHINES. I ALSO SHARE MY LEARNINGS AND EXPERIENCES ON MY </span>
 					<Button 
 						button="" 
 						buttonClassName="blog clickable pages" 
