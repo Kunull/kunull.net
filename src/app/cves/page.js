63ac0d206a84439f29043de9e2bb04827aa63777
Kunal Walavalkar <kunalw2002@gmail.com>
2024-07-09 15:06:36 +0530

    Update page.js

--- src/app/cves/page.js
+++ src/app/cves/page.js
@@ -15,20 +15,29 @@ const CVEs = () => {
 			<Navbar
 				previousPage="KUNULL"
 				previousPageClassName="tertiaryButton clickable glitch layers"
-				previousPageAriaLabel="Go to KW i.e. home page"
+				previousPageAriaLabel="Go to KUNULL i.e. home page"
 				previousPageHref="/"
+				
 				slash="&nbsp;/&nbsp;"
-				slashClassName="slash"
+				slashClassName="tertiaryText slash"
+				
 				currentPage="CVES"
 				currentPageClassName="tertiaryText currentPage"
+//				option2Page="experience clickable"
+
 				option1Page="CERTIFICATIONS"
 				option1PageClassName="certifications secondaryButton clickable"
 				option1PageAriaLabel="Go to my Certifications page"
-//				pageOption2="experience clickable"
-				option2Page="projects"
+	
+				option2Page="PROJECTS"
 				option2PageClassName="projects secondaryButton clickable"
 				option2PageAriaLabel="Go to my Projects page"
+
+				option3Page="CVES"
+				option3PageClassName="cves hidden clickable"
+				option3PageAriaLabel="Go to my CVEs page"
 			/>
+
 
       			<main className="capital">
 				<p className='spacing'>
