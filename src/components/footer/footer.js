62804c8968fdd6b00b7c5ae2955085db35820b97
Kunal Walavalkar <kunalw2002@gmail.com>
2024-07-22 13:21:24 +0530

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -18,6 +18,29 @@ const Footer = () => {
 					</li>
 
 					<li className="footerLi">
+						<Button 
+		          				button="WRITEUPS"
+							buttonClassName="write-ups secondaryButton clickable"
+							buttonHref="https://writeups-kunull.vercel.app"
+							target="_blank"
+							ariaLabel=""
+		    				/>
+	      				</li>
+	
+					<li className="footerLi">
+						<Button 
+		          				button="BLOG"
+							buttonClassName="blog secondaryButton clickable"
+							buttonHref="https://blog-kunull.vercel.app/"
+							target="_blank"
+							ariaLabel=""
+		        			/>
+					</li>								
+				</ul>
+
+								
+				<ul className="footerGridInner">
+					<li className="footerLi">
 		        			<Button 
 		    					button="GITHUB"
 							buttonClassName="github secondaryButton clickable"
@@ -26,7 +49,30 @@ const Footer = () => {
 				  			ariaLabel=""
 		  				/>
 	    				</li>
-	
+
+					<li className="footerLi">
+						<Button 
+		          				button="LINKEDIN"
+					  		buttonClassName="linkedin secondaryButton clickable"
+							buttonHref="https://www.linkedin.com/in/kunull/"
+							target="_blank"
+							ariaLabel=""
+		    				/>
+	  				</li>
+								
+				  	<li className="footerLi">
+						<Button 
+		    					button="RESUME"
+							buttonClassName="resume secondaryButton clickable"
+							buttonHref="/assets/resume/Kunal-Walavalkar-Resume.pdf"
+							target="_blank"
+							ariaLabel="Check out my resume"
+		  				/>
+					</li>
+				</ul>	
+
+								
+				<ul className="footerGridInner">				
 					<li className="footerLi">
 						<Button 
 		          				button="EMAIL"
@@ -37,57 +83,16 @@ const Footer = () => {
 		        			/>
 					</li>
 
+					<li className="footerLi">
+						<Button 
+		          				button="PGP KEY"
+					  		buttonClassName="key secondaryButton clickable"
+							buttonHref="https://kunull.vercel.app/assets/key/kunalwalavalkar_public_key.txt"
+							target="_blank"
+							ariaLabel=""
+		    				/>
+	  				</li>	
 				</ul>
-				
-				<li className="footerLi">
-					<Button 
-	          				button="WRITEUPS"
-						buttonClassName="write-ups secondaryButton clickable"
-						buttonHref="https://writeups-kunull.vercel.app"
-						target="_blank"
-						ariaLabel=""
-	    				/>
-      				</li>
-
-				<li className="footerLi">
-					<Button 
-	          				button="LINKEDIN"
-				  		buttonClassName="linkedin secondaryButton clickable"
-						buttonHref="https://www.linkedin.com/in/kunull/"
-						target="_blank"
-						ariaLabel=""
-	    				/>
-  				</li>
-
-				<li className="footerLi">
-					<Button 
-	          				button="PGP KEY"
-				  		buttonClassName="key secondaryButton clickable"
-						buttonHref="https://kunull.vercel.app/assets/key/kunalwalavalkar_public_key.txt"
-						target="_blank"
-						ariaLabel=""
-	    				/>
-  				</li>	
-							
-				<li className="footerLi">
-					<Button 
-	          				button="BLOG"
-						buttonClassName="blog secondaryButton clickable"
-						buttonHref="https://blog-kunull.vercel.app/"
-						target="_blank"
-						ariaLabel=""
-	        			/>
-				</li>
-
-			  	<li className="footerLi">
-					<Button 
-	    					button="RESUME"
-						buttonClassName="resume secondaryButton clickable"
-						buttonHref="/assets/resume/Kunal-Walavalkar-Resume.pdf"
-						target="_blank"
-						ariaLabel="Check out my resume"
-	  				/>
-				</li>
   			</ul>
     		</footer>
 	);
