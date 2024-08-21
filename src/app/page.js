06d9dbee05d16f4e32df3b846c5add1bcf33c10a
Kunal Walavalkar <kunalw2002@gmail.com>
2024-08-21 16:51:03 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -120,16 +120,18 @@ const Home = () => {
 						buttonHref="https://matrix.to/#/@kunull:matrix.org"
 						target="_blank"
 						ariaLabel=""
-					/>							
-					<span className="primaryText"> IF YOU'D LIKE TO GET IN TOUCH. YOU CAN USE MY PUBLIC </span>
-					<Button
-						button="PGP KEY"
-						buttonClassName="key primaryButton clickable"
-						buttonHref="/assets/key/kunalwalavalkar_public_key.txt"
-						target="_blank"
-						ariaLabel=""
-					/>
-					<span className="primaryText"> FOR ENCRYPTION.</span>
+					/>			
+					<span className="primaryText">.</span>
+							
+					// <span className="primaryText"> IF YOU'D LIKE TO GET IN TOUCH. YOU CAN USE MY PUBLIC </span>
+					// <Button
+					// 	button="PGP KEY"
+					// 	buttonClassName="key primaryButton clickable"
+					// 	buttonHref="/assets/key/kunalwalavalkar_public_key.txt"
+					// 	target="_blank"
+					// 	ariaLabel=""
+					// />
+					// <span className="primaryText"> FOR ENCRYPTION.</span>
 				</p>
 			</main>
 
