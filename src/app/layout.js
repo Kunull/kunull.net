8b8951ce39ccbd020c4bc55ca576d9f5d61f6cc5
Kunal Walavalkar <kunalw2002@gmail.com>
2025-03-31 17:25:08 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,33 +14,11 @@ export const viewport = {
   themeColor: "#000",
 };
 
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={myFont.className}>
-//         <PageTransition> 
-//           <div className="sticky">
-//             <div className="effectGlitch"></div>
-//             <div className="effectNoise"></div>
-//             <div className="scanLine"></div>
-//             <div className="gradient"></div>
-//             <div className="noise"></div>
-//             <div className="backgroundLines"></div>
-//             <div className="backgroundEffect"></div>
-//           </div>
-
-//           {children}
-//         </PageTransition>
-//       </body>
-//     </html>
-//   )
-// }
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={myFont.className}>
-        {/* <PageTransition> */}
+        <PageTransition> 
           <div className="sticky">
             <div className="effectGlitch"></div>
             <div className="effectNoise"></div>
@@ -52,8 +30,30 @@ export default function RootLayout({ children }) {
           </div>
 
           {children}
-        {/* </PageTransition> */}
+        </PageTransition>
       </body>
     </html>
   )
 }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={myFont.className}>
+//         {/* <PageTransition> */}
+//           <div className="sticky">
+//             <div className="effectGlitch"></div>
+//             <div className="effectNoise"></div>
+//             <div className="scanLine"></div>
+//             <div className="gradient"></div>
+//             <div className="noise"></div>
+//             <div className="backgroundLines"></div>
+//             <div className="backgroundEffect"></div>
+//           </div>
+
+//           {children}
+//         {/* </PageTransition> */}
+//       </body>
+//     </html>
+//   )
+// }
