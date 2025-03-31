1fae47e7ea92d98fbd074d6677d28dd4def823a7
Kunal Walavalkar <kunalw2002@gmail.com>
2025-03-31 17:23:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from 'next/font/local'
 import './globals.css'
-import PageTransition from '../components/PageTransition/PageTransition'
+// import PageTransition from '../components/PageTransition/PageTransition'
 
 const myFont = localFont({ src: '../../public/assets/fonts/VCR OSD Mono/VCROSDMono.woff2' })
 
@@ -14,11 +14,33 @@ export const viewport = {
   themeColor: "#000",
 };
 
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={myFont.className}>
+//         <PageTransition> 
+//           <div className="sticky">
+//             <div className="effectGlitch"></div>
+//             <div className="effectNoise"></div>
+//             <div className="scanLine"></div>
+//             <div className="gradient"></div>
+//             <div className="noise"></div>
+//             <div className="backgroundLines"></div>
+//             <div className="backgroundEffect"></div>
+//           </div>
+
+//           {children}
+//         </PageTransition>
+//       </body>
+//     </html>
+//   )
+// }
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={myFont.className}>
-        <PageTransition> 
+        {/* <PageTransition> */}
           <div className="sticky">
             <div className="effectGlitch"></div>
             <div className="effectNoise"></div>
@@ -30,7 +52,7 @@ export default function RootLayout({ children }) {
           </div>
 
           {children}
-        </PageTransition>
+        {/* </PageTransition> */}
       </body>
     </html>
   )
