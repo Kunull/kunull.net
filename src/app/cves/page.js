b342d60f678ef487b2cdba482f4a5f122d2b6d95
Kunal Walavalkar <kunalw2002@gmail.com>
2025-11-18 13:02:06 +0530

    Update page.js

--- src/app/cves/page.js
+++ src/app/cves/page.js
@@ -3,9 +3,9 @@ import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 
 export const metadata = {
-  title: 'Kunull | CVEs',
+  title: 'Kunull // CVEs',
   openGraph: {
-    title: 'Kunull | CVEs',
+    title: 'Kunull // CVEs',
   },
 }
 
